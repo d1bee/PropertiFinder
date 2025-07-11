731c9a4160a4260f0c7386ee74f56d887b0125d0
alexa <mekirandy@gmail.com>
2025-07-11 09:08:28 +0000

    ganti untuk ikon rumah seperti ini https://icon-icons.com/id/icon/rumah/

--- src/components/property-markers.tsx
+++ src/components/property-markers.tsx
@@ -16,45 +16,41 @@ const getIcon = (type: Property['type']): google.maps.Icon => {
         return {}; 
     }
 
-    // Using SVG paths for custom icons
     const ICONS = {
       Rumah: { 
-        path: 'M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5-10-5-10 5z', // A simple house shape
+        path: 'M12 3L4 9v12h16V9L12 3z',
         color: '#10B981', 
-        scale: 0.8 
+        scale: 1 
       },
       Apartemen: { 
-        path: 'M12 2L2 7v10l10 5 10-5V7L12 2zm8 14.5l-8 4-8-4v-8.5l8-4 8 4v8.5zM10 11h4v6h-4v-6z', // Building
+        path: 'M12 2L2 7v10l10 5 10-5V7L12 2zm8 14.5l-8 4-8-4v-8.5l8-4 8 4v8.5zM10 11h4v6h-4v-6z',
         color: '#3B82F6', 
         scale: 0.8 
       },
       'Tanah Kosong': { 
-        path: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14H9v-2h2v2zm0-4H9V7h2v5zm4 4h-2v-2h2v2zm0-4h-2V7h2v5z', // Placeholder for land (leaf/plant)
-        path2: 'M12 2L4 5v14h16V5l-8-3zM6 7h12v10H6V7z',
-        path3: 'M14.6,3.4C14.2,3.1,13.7,3,13.2,3H4C3.4,3,3,3.4,3,4v16c0,0.6,0.4,1,1,1h16c0.6,0,1-0.4,1-1V9.8c0-0.5-0.1-1-0.4-1.4L14.6,3.4z M19,19H5V5h7v5h5V19z',
-        path: 'M2,22h20V2H2V22z M13,4h6v6h-6V4z M13,12h6v6h-6V12z M4,4h7v16H4V4z',
+        path: 'M2 22h20V2H2v20zM14 4h4v4h-4V4zM8 4h4v4H8V4zM14 10h4v4h-4v-4zM8 10h4v4H8v-4zM4 18h16v-2H4v2zm0-4h4v-4H4v4zm0-6h4V4H4v4z',
         color: '#F97316', 
-        scale: 0.8 
+        scale: 0.9 
       },
       Gudang: { 
-        path: 'M20 6H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-2 12H6v-4h12v4zm0-6H6V9h12v3z', // Warehouse
+        path: 'M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM8 18H6v-4h2v4zm4 0h-2v-4h2v4zm4 0h-2v-4h2v4zm0-6H6V8h12v4z',
         color: '#6B7280', 
-        scale: 0.8 
+        scale: 0.9
       },
       Ruko: { 
-        path: 'M12 3L2 8v11h20V8L12 3zm6 14h-4v-4h-4v4H6v-6.5l6-4.5 6 4.5V17z', // Shophouse
+        path: 'M12 3L2 8v11h20V8L12 3zm6 14h-4v-4h-4v4H6v-6.5l6-4.5 6 4.5V17z',
         color: '#A855F7', 
-        scale: 0.8 
+        scale: 1 
       },
       'Galangan Kapal': { 
-        path: 'M21 6h-2v9H5v-9H3c-1.1 0-2 .9-2 2v9c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2z', // Shipyard/Anchor
+        path: 'M12 2L4 6v8c0 2.21 1.79 4 4 4h8c2.21 0 4-1.79 4-4V6l-8-4zM6 14V7.5l6 3.5 6-3.5V14H6z',
         color: '#06B6D4', 
-        scale: 0.8 
+        scale: 1 
       },
       Pabrik: { 
-        path: 'M2 22h20V10L12 2 2 10v12zm4-2h3v-7h6v7h3V10l-6-4.5L6 10v10z', // Factory
+        path: 'M2 22h20V10L12 2 2 10v12zm4-2h3v-7h6v7h3V10l-6-4.5L6 10v10z',
         color: '#4B5563', 
-        scale: 0.8 
+        scale: 1 
       },
     };
 
@@ -67,7 +63,7 @@ const getIcon = (type: Property['type']): google.maps.Icon => {
         strokeColor: 'white',
         strokeWeight: 1.5,
         scale: iconConfig.scale,
-        anchor: new google.maps.Point(12, 12), // Center the icon
+        anchor: new google.maps.Point(12, 12),
         rotation: 0
     };
 };
